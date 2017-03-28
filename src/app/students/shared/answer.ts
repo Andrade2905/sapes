445a8cee3eb248ed790d73a124a2e4bc21f9a6da
Leonardo Victor <leonardovff@gmail.com>
2017-03-28 10:54:00 -0300

    Primeira versão de produção

--- src/app/students/shared/answer.ts
+++
@@ -1,9 +0,0 @@
-export interface Answer {
-  id: number;
-  alternative_id: number;
-  alternative_flag: boolean;
-  complement: string;
-  phase: number;
-  question_id: number;
-  student_id: number;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  urlToApi: "https://sapesapi.nitrofull.com.br/api/",
-  urlToOauthToken: "https://sapesapi.nitrofull.com.br/oauth/token"
-};

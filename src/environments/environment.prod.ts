0dc4348952497e74e2e2d9e6d5c0f728cf92eb65
LeonardoVFF <leonardovff@gmail.com>
2017-04-06 11:21:42 -0300

    Link para notion em produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   urlToApi: "//sapesapi.senai.br/api/",
-  urlToOauthToken: "//sapesapi.senai.br/oauth/token"
+  urlToOauthToken: "//sapesapi.senai.br/oauth/token",
+  urlToNotionManual: "https://www.notion.so/MANUAL-DO-SISTEMA-96d289598fb649dd89835d191a15396b"
 };

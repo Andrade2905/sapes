2202d37b6ef79866e46ae4793a741124f0095362
LeonardoVFF <leonardovff@gmail.com>
2017-04-06 11:08:00 -0300

    Correção do link de enviroment

--- main.bundle.js
+++ main.bundle.js
@@ -2884,14 +2884,22 @@ var StudentsRoutingModule = (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
+// // The file contents for the current environment will overwrite these during build.
+// // The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// // `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// // The list of which env maps to which file can be found in `angular-cli.json`.
+// export const environment = {
+//   production: false,
+//   urlToApi: "https://sapesapi.al.senai.br/api/",
+//   urlToOauthToken: "https://sapesapi.al.senai.br/oauth/token",
+//   urlToNotionManual: "https://www.notion.so/MANUAL-DO-SISTEMA-96d289598fb649dd89835d191a15396b"
+//   // urlToApi: "http://10.83.3.191/api/",
+//   // urlToOauthToken: "http://10.83.3.191/oauth/token"
+// };
 var environment = {
-    production: false,
-    urlToApi: "https://sapesapi.al.senai.br/api/",
-    urlToOauthToken: "https://sapesapi.al.senai.br/oauth/token",
+    production: true,
+    urlToApi: "//sapesapi.senai.br/api/",
+    urlToOauthToken: "//sapesapi.senai.br/oauth/token",
     urlToNotionManual: "https://www.notion.so/MANUAL-DO-SISTEMA-96d289598fb649dd89835d191a15396b"
 };
 //# sourceMappingURL=/home/leonardovff/dev/sapes/src/environment.js.map

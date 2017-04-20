b13b1792c4e4f2970d02a9f3f785cb72d9353adb
Rodrigo Vasconcelos <andrade2905@gmail.com>
2017-04-20 11:43:11 -0300

    View semi completo

--- src/app/students/students-view/students-view.component.ts
+++ src/app/students/students-view/students-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { bdInfo } from '../students-form/data';
 
 import { StudentsService } from './../shared/students.service';
 
@@ -9,7 +10,14 @@ import { StudentsService } from './../shared/students.service';
   styleUrls: ['./students-view.component.css']
 })
 export class StudentsViewComponent implements OnInit {
-  student;
+  private student;
+  private situacao;
+  private remuneracao;
+  private sexo;
+  private origin;
+  private mesInicio;
+  private mesFim;
+  private bolsas;
 
   constructor(
     private route: ActivatedRoute, 
@@ -25,8 +33,34 @@ export class StudentsViewComponent implements OnInit {
       .subscribe(
         student => {
           this.student = student;
+          console.log (this.student); 
+        
+          this.situacao = bdInfo.estagios.filter(situacao => 
+            situacao.id === student.answers[1].alternative_id
+          )[0];
 
-          console.log(this.student);
+
+          this.remuneracao = bdInfo.salary.filter(remuneracao =>
+            remuneracao.value === student.answers[2].alternative_id
+          )[0];
+
+          this.bolsas = bdInfo.pronatec.filter(bolsas =>
+            bolsas.id === student.pronatec.id
+          )[0];
+          
+          this.sexo = bdInfo.genders.filter(sexo => 
+            sexo.id === student.gender
+          )[0];
+
+          this.origin = this.student.origin == 2 ? "Pública": "Particular";
+          
+          this.mesInicio = bdInfo.months.filter(mes => 
+            mes.id === student.start_month
+          )[0];
+
+          this.mesFim = bdInfo.months.filter(mes => 
+            mes.id === student.end_month
+          )[0];
         },
         response => {
           if (response.status == 404) {
